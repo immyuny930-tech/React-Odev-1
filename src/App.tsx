6556bb69e42ad8eaef343674f0918a4cb20c791b
Imraan Unal <imraanunal930@gmail.com>
2026-07-11 16:16:27 +0300

    Yemekkarti.tsx componentı eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Header from "./components/Header";
+import Kart from "./components/Yemekkarti";
 
 function App() {
   return (
     <div>
       <Header />
+      <Kart 
+        yemekAdi="Mantı" 
+        pisirmeSuresi={2} 
+        zorluk="Zor" 
+        vejetaryan={false}
+        malzemeler={[
+        "Un",
+        "Su",
+        "Yumurta",
+        "Tuz",
+        "Kıyma",
+        "Soğan",
+        "Yoğurt"
+        ]} />
     </div>
   );
 }
---
+++ src/components/Yemekkarti.tsx
@@ -0,0 +1,27 @@
+interface KartProps {
+    yemekAdi: string;
+    pisirmeSuresi: number;
+    zorluk: string;
+    vejetaryan: boolean;
+    malzemeler: string[];
+}
+
+function Kart(props: KartProps) {
+    return(
+        <div>
+            <h1>{props.yemekAdi}</h1>
+            <p>Pişirme Süresi: {props.pisirmeSuresi} Saat</p>
+            <p>Pişirme Zorluğu: {props.zorluk}</p>
+            <p>Vejetaryan: {props.vejetaryan ? "Evet" : "Hayır"}</p>
+            <h3>Malzemeler:</h3>
+            <ul>
+                {props.malzemeler.map((malzeme) => (
+                    <li>{malzeme}</li>
+                ))}
+            </ul>
+        </div>
+
+    )
+}
+
+export default Kart;
