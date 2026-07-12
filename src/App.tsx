64f037448c228f08051a8b46f1c45603fe3ed52e
Imraan Unal <imraanunal930@gmail.com>
2026-07-12 15:37:32 +0300

    Container.tsx componentı eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,40 @@
 import Header from "./components/Header";
 import Kart from "./components/Yemekkarti";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div>
       <Header />
-      <Kart 
-        yemekAdi="Mantı" 
-        pisirmeSuresi={2} 
-        zorluk="Zor" 
-        vejetaryan={false}
-        malzemeler={[
-        "Un",
-        "Su",
-        "Yumurta",
-        "Tuz",
-        "Kıyma",
-        "Soğan",
-        "Yoğurt"
-        ]}/>
+      <Container>
         <Kart 
-        yemekAdi="Mercimek Çorbası"
-        pisirmeSuresi={1}
-        zorluk="Kolay"
-        vejetaryan={true}
-        malzemeler={[
-          "Mercimek",
-          "Soğan",
-          "Havuç",
+          yemekAdi="Mantı" 
+          pisirmeSuresi={2} 
+          zorluk="Zor" 
+          vejetaryan={false}
+          malzemeler={[
+          "Un",
           "Su",
+          "Yumurta",
           "Tuz",
-          "Biber"
-        ]}/>
+          "Kıyma",
+          "Soğan",
+          "Yoğurt"
+          ]}/>
+          <Kart 
+          yemekAdi="Mercimek Çorbası"
+          pisirmeSuresi={1}
+          zorluk="Kolay"
+          vejetaryan={true}
+          malzemeler={[
+            "Mercimek",
+            "Soğan",
+            "Havuç",
+            "Su",
+            "Tuz",
+            "Biber"
+            ]} />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+function Container(props: ContainerProps) {
+    return (
+        <div>
+            {props.children}
+        </div>
+    );
+}
+
+export default Container;
